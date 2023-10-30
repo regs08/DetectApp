28a6c5061e78b3579e118ff1c03b85fb2bc3f081
cregs08 <cole.regnier@gmail.com>
2023-10-30 19:50:51 +0800

    chaning ip to static ip of pi

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ DETECTION_TOPIC = "vehicle/detections"
 IMAGE_TOPIC = "vehicle/image"
 #
 
-# set the IP address for the rpi look at the last chat gpt chat
+# set the IP address for the rpi look at the last chat gpt cha
 # upload to git and test
