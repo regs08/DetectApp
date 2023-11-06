dec21d70c66c9e43317101e9cfdeb2cab0a86fb1
cregs08 <cole.regnier@gmail.com>
2023-11-06 20:25:14 +0800

    chaning ip to static ip of pi

--- app/config.py
+++ app/config.py
@@ -1,8 +1,6 @@
 # MQTT Constants
-BROKER_ADDRESS = "192.168.1.101"
+BROKER_ADDRESS = "localhost"
 DETECTION_TOPIC = "vehicle/detections"
 IMAGE_TOPIC = "vehicle/image"
+PING_TOPIC = "ping"
 #
-
-# set the IP address for the rpi look at the last chat gpt cha
-# upload to git and test
