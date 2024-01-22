e6b5a7bfe6e99bb70f1254a6a90068a5a13e24b7
cregs08 <cole.regnier@gmail.com>
2024-01-22 17:36:08 +1030

    had issues with setting model path as an env var going easy way for now

--- set_model_path.py
+++ set_model_path.py
@@ -1,7 +1,11 @@
 import os
+# Todo get the model path set or configure it with mqtt
 
+
+grape_model_file = 'grape_trunk_detect_ssd.tflite'
+pretrained_coco_model_file = 'efficientdet_lite0.tflite'
 current_file_path = os.path.abspath(__file__)
 current_dir = os.path.dirname(current_file_path)
-default_model_path = os.path.join(current_dir, 'saved_models', 'grape_trunk_detect_ssd.tflite')
-model_path = os.getenv('MODEL_PATH', 'default_model_path')
+default_model_path = os.path.join(current_dir, 'saved_models', grape_model_file)
+model_path = default_model_path#os.getenv('MODEL_PATH', 'default_model_path')
 
