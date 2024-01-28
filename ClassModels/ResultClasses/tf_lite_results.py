cc4bae0d5627de55a2f49c1e8df830b4c4823c0d
cregs08 <cole.regnier@gmail.com>
2024-01-28 10:38:46 +1030

    updated constructor sig

--- ClassModels/ResultClasses/tf_lite_results.py
+++ ClassModels/ResultClasses/tf_lite_results.py
@@ -1,18 +1,19 @@
 import numpy as np
 from ClassModels.ResultClasses.results_base import ResultsBase
+from typing import List
 
 
 class TFLiteResults(ResultsBase):
-    def __init__(self, boxes: np.ndarray, labels: list[str], confs: list[float]):
+    def __init__(self, boxes: List[np.ndarray], labels: List[str], confs: List[float]):
         """
         Initialize the TFLiteResults with detection output.
 
-        :param boxes: A NumPy array of bounding boxes, each box represented as [xmin, ymin, xmax, ymax].
+        :param boxes: A list of NumPy arrays, each array is a bounding box represented as [xmin, ymin, xmax, ymax].
         :param labels: A list of labels corresponding to the detected objects.
         :param confs: A list of confidence scores corresponding to the detected objects.
         """
-        if not isinstance(boxes, np.ndarray):
-            raise TypeError("boxes must be a NumPy array")
+        if not all(isinstance(box, np.ndarray) and box.shape == (4,) for box in boxes):
+            raise TypeError("boxes must be a list of NumPy arrays with shape (4,)")
         if not all(isinstance(label, str) for label in labels):
             raise TypeError("labels must be a list of strings")
         if not all(isinstance(conf, float) for conf in confs):
