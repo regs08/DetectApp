e5cdce9b943e3ca156d1d684717f410613fae87c
cregs08 <cole.regnier@gmail.com>
2024-01-28 21:09:54 +1100

    chaning constructor to empty

--- ClassHandlers/FrameAnnotator/frame_annotator_detections.py
+++ ClassHandlers/FrameAnnotator/frame_annotator_detections.py
@@ -3,38 +3,37 @@ from ClassModels.ResultClasses.tf_lite_results import TFLiteResults
 import numpy as np
 import supervision as sv
 
+
 class FrameAnnotatorDetections(FrameAnnotatorBase):
-    def __init__(self, results: TFLiteResults):
-        """
-        Initialize the FrameAnnotatorDetections with detection results.
+    def __init__(self):
 
-        :param results: An instance of TFLiteResults containing bounding boxes, labels, and confidence scores.
-        """
-        super().__init__(results)  # Initialize the superclass with the results
+        super().__init__()  # Initialize the superclass with the results
 
-    def annotate_frame(self, frame):
+    def annotate_frame(self, results: TFLiteResults, frame):
         """
         Annotate the given frame with detection information.
 
         :param frame: The frame to be annotated.
         :return: The frame with annotations (bounding boxes and labels).
         """
+        if not isinstance(results, TFLiteResults):
+            raise TypeError("reults not of TFLiteResults!")
         # Calculate and display the frames per second on the frame
         self.calculate_and_show_fps(frame)
 
         # Check if there are any detections to annotate
-        if len(self.results.boxes) > 0:
+        if len(results.boxes) > 0:
             # Convert bounding boxes to a NumPy array
-            np_bboxes = np.array(self.results.boxes)
+            np_bboxes = np.array(results.boxes)
 
             # Create Detections object with bounding boxes and confidence scores
-            detections = sv.Detections(xyxy=np_bboxes, confidence=np.array(self.results.confs))
+            detections = sv.Detections(xyxy=np_bboxes, confidence=np.array(results.confs))
 
             # Initialize a BoxAnnotator for drawing bounding boxes
             box_annotator = sv.BoxAnnotator()
 
             # Combine labels with confidence scores for display
-            labels_with_conf = [f"{l} {c:.2f}" for l, c in zip(self.results.labels, self.results.confs)]
+            labels_with_conf = [f"{l} {c:.2f}" for l, c in zip(results.labels, results.confs)]
 
             # Annotate the frame with bounding boxes and labels
             annotated_frame = box_annotator.annotate(frame, detections, labels=labels_with_conf)
