3d25ec9d149fd3100e2f000f93f2512183e48317
cregs08 <cole.regnier@gmail.com>
2024-01-29 20:17:53 +1100

    adding comments

--- model_logic/base_model.py
+++ model_logic/base_model.py
@@ -1,20 +1,35 @@
 from abc import ABC, abstractmethod
-
 from ClassModels.Configs.model_config import ModelConfig
-
 
 class BaseModel(ABC):
     """A base class for real-time vision model logic using a camera (default webcamera)"""
 
     def __init__(self, model_config: ModelConfig):
-        self.model_config = model_config
-        self.initialize_model()
+        """
+        Initializes the BaseModel with a given configuration.
+
+        Args:
+            model_config (ModelConfig): Configuration settings for the model.
+        """
+        self.model_config = model_config  # Store the configuration settings
+        self.initialize_model()  # Call to the method to initialize the model
 
     @abstractmethod
     def initialize_model(self):
-        """Initialize the model. Must be overridden by subclasses."""
+        """
+        Abstract method to initialize the model.
+        This method must be implemented by all subclasses of BaseModel.
+        """
         pass
 
     @abstractmethod
     def process_frame(self, frame):
+        """
+        Abstract method to process a single frame.
+        This method must be implemented by all subclasses and is intended to
+        contain the logic for processing each frame captured from the camera.
+
+        Args:
+            frame: The frame to be processed.
+        """
         pass
